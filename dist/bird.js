956cb85c72426c582f5b7ef53d40d791d7c143c8
GGICE <i@ice.gs>
2016-08-14 02:39:43 +0800

    Test build

---
+++ dist/bird.js
@@ -0,0 +1,1 @@
+!function(e){function t(o){if(r[o])return r[o].exports;var a=r[o]={exports:{},id:o,loaded:!1};return e[o].call(a.exports,a,a.exports,t),a.loaded=!0,a.exports}var r={};return t.m=e,t.c=r,t.p="",t(0)}([function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r;r={template:"",data:{}},t["default"]=r}]);
