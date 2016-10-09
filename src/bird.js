a2304608c1c81670e14894c9082dc63a0877df01
GGICE <i@ice.gs>
2016-10-10 00:15:42 +0800

    Add some test.

--- src/bird.js
+++ src/bird.js
@@ -1,8 +1,23 @@
 var Bird
 
 Bird = {
-  template: '',
-  data: {}
+  create(options) {
+    var { template, el , data } = options
+
+    this.wrap =  document.querySelect(el)
+    this.template = template
+    this.el = el
+    this.data = data
+
+    this.parse()
+    this.apply()
+  },
+  parse() {
+
+  },
+  apply() {
+    
+  }
 }
 
 export default Bird
